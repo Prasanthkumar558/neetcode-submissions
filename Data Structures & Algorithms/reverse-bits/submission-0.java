0044201c2b9a7beab49c9bd9746dd7ea3b0ffd50
Prasanth kumar Gundumalla <gandlaprasanthkumar@gmail.com>
2026-05-15 09:55:02 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseBits(int n) {
+        
+        int ans = Integer.reverse(n) ;// it is a inbuilt method 
+        int reverse = 0;
+
+        for(int i=0;i < 32;i++){
+            reverse = reverse << 1;
+            reverse = reverse | n & 1;
+            n = n >>> 1;
+        }
+
+        return reverse;
+    }
+}
