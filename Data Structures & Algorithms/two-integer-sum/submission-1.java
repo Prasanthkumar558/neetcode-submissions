6acc58566c792b1a3c8e941443114d6f2e43dbc4
Prasanth kumar Gundumalla <gandlaprasanthkumar@gmail.com>
2026-08-29 20:34:11 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+        public int[] twoSum(int[] nums, int target) {
+                
+                        int[] sol = {-1,-1};
+                                if(nums.length<2){
+                                            return sol;
+                                                    }
+                                                            HashMap<Integer,Integer> mp = new HashMap<>();
+                                                                    for(int i=0;i<nums.length;i++){
+                                                                                int n = target-nums[i];
+                                                                                            if(mp.containsKey(n)){
+                                                                                                            int j=mp.get(n);
+                                                                                                                            sol[0]=j;
+                                                                                                                                            sol[1]=i;
+                                                                                                                                                        }
+                                                                                                                                                                    else{
+                                                                                                                                                                                    mp.put(nums[i],i);
+                                                                                                                                                                                                }
+                                                                                                                                                                                                        }
+                                                                                                                                                                                                                return sol;
+                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                    }
