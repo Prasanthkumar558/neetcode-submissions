e32fe256db72bd0dbc7e0a184c651a282a1e948a
Prasanth kumar Gandla <gandlaprasanthkumar@gmail.com>
2026-09-10 17:10:20 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.java
@@ -0,0 +1,46 @@
+class MyQueue {
+
+    public Stack<Integer> st;
+    public Stack<Integer> temp;
+    public MyQueue() {
+        st = new Stack<>();
+        temp = new Stack<>();
+    }
+    
+    public void push(int x) {
+        
+        while(!st.isEmpty()) {
+            temp.push(st.pop());
+        }
+
+        st.push(x);
+
+        while(!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+    }
+    
+    public int pop() {
+        
+        return st.pop();
+    }
+    
+    public int peek() {
+        
+        return st.peek();
+    }
+    
+    public boolean empty() {
+        
+        return st.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
